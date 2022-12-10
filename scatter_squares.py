120361dd2b2dfab26b8256275da6eb4c94cf3656
c2065535 <D.Leivdal2@newcastle.ac.uk>
2022-12-10 15:45:18 +0000

    Add range for each axis

--- scatter_squares.py
+++ scatter_squares.py
@@ -12,6 +12,9 @@ ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
+
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
