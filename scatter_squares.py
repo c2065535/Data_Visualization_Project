9b06b2a31f73fdc593a91e2133ae1a683cf9173c
c2065535 <D.Leivdal2@newcastle.ac.uk>
2022-12-10 15:49:21 +0000

    Add colormap Add option to save plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,8 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axis.
 ax.set_title("Square Numbers", fontsize=24)
@@ -19,3 +20,4 @@ ax.axis([0, 1100, 0, 1100000])
 ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
+# plt.savefig('squares_plot.png')
