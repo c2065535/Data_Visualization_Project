75cfa0661287ece922d5d25f984f634a792d5191
c2065535 <D.Leivdal2@newcastle.ac.uk>
2022-12-11 14:16:44 +0000

    Add start and end point Remove Axes

--- rw_visual.py
+++ rw_visual.py
@@ -9,9 +9,19 @@ while True:
     rw.fill_walk()
 
     # Plot the points in the walk.
-
     fig, ax = plt.subplots()
     ax.scatter(rw.x_values, rw.y_values, s=5)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Emphasise the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
