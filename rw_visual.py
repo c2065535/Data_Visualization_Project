6caa51d8e44ff4b32294ced2479a23f6425dd8c6
c2065535 <D.Leivdal2@newcastle.ac.uk>
2022-12-11 14:21:15 +0000

    Alter fig size

--- rw_visual.py
+++ rw_visual.py
@@ -5,14 +5,14 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active
 while True:
     # Make random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     ax.scatter(rw.x_values, rw.y_values, s=5)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # Emphasise the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
